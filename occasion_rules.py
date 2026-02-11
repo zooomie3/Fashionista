7954274be38670e446f717ce073b03cd8e970b75
LiskeJ <bijouxliske@gmail.com>
2026-02-11 15:59:23 +0100

    Add occasion rules for clothing recommendations

---
+++ occasion_rules.py
@@ -0,0 +1,10 @@
+# Identifying occasions and giving them output
+occasion_rules = {
+    "night_out": ["short skirt", "cute top", "heels", "boots", "small bag", "jeans", "long-sleeve", "jacket"],
+    "family dinner": ["sweater", "jeans", "cardigan", "top", "long-sleeve", "long skirt", "sneakers"],
+    "casual": ["jeans", "sneakers", "top", "hoodie", "sweater", "cardigan", "cargo pants", "long sleeve", "zip up", "uggs", "rainboots", "fuzzy slippers"],
+    "gym": ["tank top", "hoodie", "sports shorts", "leggings", "sweatpants", "sneakers", "running shoes"],
+    "formal": ["dress pants", "blazer", "blouse", "shirt (button-up)", "tie", "formal skirt", "closed heels", "dress-up shoes", "mary janes"], #change shirt (button-up) later
+    "festival": ["cowboy hat", "cowboy boots", "bandana", "raincoat", "rainboots", "funky/colorful tops", "shorts", "pants", "sandals", "sneakers", "boots", "sun hat"], #change funky/colorful later
+    "beach": ["bikini top", "bikini bottom", "shorts", "top", "flowy", "flip-flops", "sandals", "sun hat", "sunglasses"] #change bikini top and bottom to 'bikini' or relate them later
+}
