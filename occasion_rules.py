2af6e46e9751a0e5f58ac082eccbfc9cb74d7086
LiskeJ <bijouxliske@gmail.com>
2026-02-11 22:09:14 +0100

    Revise occasion rules with updated clothing names
    Updated occasion rules with new clothing items for various occasions.

--- occasion_rules.py
+++ occasion_rules.py
@@ -1,10 +1,15 @@
 # Identifying occasions and giving them output
 occasion_rules = {
-    "night_out": ["short skirt", "cute top", "heels", "boots", "small bag", "jeans", "long-sleeve", "jacket"],
-    "family dinner": ["sweater", "jeans", "cardigan", "top", "long-sleeve", "long skirt", "sneakers"],
-    "casual": ["jeans", "sneakers", "top", "hoodie", "sweater", "cardigan", "cargo pants", "long sleeve", "zip up", "uggs", "rainboots", "fuzzy slippers"],
-    "gym": ["tank top", "hoodie", "sports shorts", "leggings", "sweatpants", "sneakers", "running shoes"],
-    "formal": ["dress pants", "blazer", "blouse", "shirt (button-up)", "tie", "formal skirt", "closed heels", "dress-up shoes", "mary janes"], #change shirt (button-up) later
-    "festival": ["cowboy hat", "cowboy boots", "bandana", "raincoat", "rainboots", "funky/colorful tops", "shorts", "pants", "sandals", "sneakers", "boots", "sun hat"], #change funky/colorful later
-    "beach": ["bikini top", "bikini bottom", "shorts", "top", "flowy", "flip-flops", "sandals", "sun hat", "sunglasses"] #change bikini top and bottom to 'bikini' or relate them later
+    "night_out": ["BOT_02", "TOP_07", "TOP_05", "SHO_04", "SHO_06", "small bag", "BOT_10", "TOP_09", "OUT_02", "TOP_14", "OUT_01"],
+    "family dinner": ["TOP_12", "BOT_10", "TOP_11", "TOP_03", "TOP_04", "TOP_09", "BOT_03", "SHO_03", "OUT_01"],
+    "casual": ["BOT_10", "SHO_03", "TOP_02", "TOP_06", "TOP_13", "TOP_12", "TOP_11", "BOT_11", "TOP_08", "TOP_14", "SHO_07", "SHO_06"],
+    "gym": ["TOP_02", "TOP_13", "BOT_05", "BOT_08", "BOT_09", "SHO_03", "running shoes"], 
+    "formal": ["BOT_12", "blazer", "OUT_04", "TOP_10", "tie", "BOT_03", "SHO_04", "SHO_05", "SHO_06"],
+    "festival": ["cowboy hat", "leather jacket", "cowboy boots", "bandana", "OUT_03", "funky/colorful tops", "BOT_06", "BOT_11", "BOT_04", "SHO_02", "SHO_03", "SHO_06", "ACC_02"], #add flared jeans!!
+    "beach": ["TOP_01", "BOT_01", "BOT_06", "TOP_05", "SHO_01", "SHO_02", "ACC_02", "ACC_03"]
 }
+
+# add some accessoiries later
+# add if rule for raincoat and rain boots etc.
+# add outerwear later as well
+# add flared jeans
